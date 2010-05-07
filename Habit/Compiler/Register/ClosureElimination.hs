88fc86a8005f90c642ab77b9624fdf627cdf14bf
Justin Bailey <justinb@cs.pdx.edu>
2010-05-07 09:58:08 -0700

    Closure elimination, attempt two.
    First pass - recognizing which procedures might return closures. It
    compiles but is not yet tested.

--- Habit/Compiler/Register/ClosureElimination.hs
+++ Habit/Compiler/Register/ClosureElimination.hs
@@ -1,129 +1,99 @@
-{-# LANGUAGE GADTs, NamedFieldPuns  #-}
+{-# LANGUAGE GADTs, NamedFieldPuns, RankNTypes  #-}
 {-# OPTIONS_GHC -Wall #-}
 module Habit.Compiler.Register.ClosureElimination (cloOpt)
 
 where
 
 import Compiler.Hoopl
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Maybe (fromMaybe, fromJust, isJust)
+import Data.Maybe (fromMaybe)
 
 import qualified Habit.Compiler.Register.Machine as H (Reg, Instr(..), Label)
 import Habit.Compiler.Register.Hoopl
 
-data CloInfo = MkClo H.Label Int | Other
+data Val = Unknown | Other | Clo H.Label Int
   deriving (Eq, Show)
 
-data CloFact = CloFact { currLabel :: Maybe (H.Label, Label) -- ^ Track currently being analyzed.
-                       , closureRefs :: Map H.Reg CloInfo -- ^ Map the registers holding "constant" closures to those closures.
-                       , allClosures :: Map H.Label CloInfo  } -- ^ Map program labels to the closures they make, if any. 
+-- | Indicates if a procedure returns a closure
+-- or something else.
+data ProcFact = Proc H.Reg Val | NoInfo
   deriving (Eq, Show)
 
-emptyFact :: CloFact 
-emptyFact = CloFact { currLabel = Nothing
-                    , closureRefs = Map.empty
-                    , allClosures = Map.empty }
-            
 cloOpt :: Body InstrNode -> FuelMonad (Body InstrNode)
-cloOpt body = do
-  let fwd = FwdPass { fp_lattice = cloLattice
-                       , fp_transfer = cloTransfer 
-                       , fp_rewrite = cloRewrite }
-      -- Initial map of labels to initial facts.
-      initFacts = zip (map fst (bodyList body)) (repeat emptyFact)
-  (body', _) <- analyzeAndRewriteFwd fwd body (mkFactBase initFacts)
-  return body'
+cloOpt body = return body
 
-cloLattice :: DataflowLattice CloFact
-cloLattice = DataflowLattice { fact_name = "Closure elimination"
-                              , fact_bot = emptyFact
-                              , fact_extend = extendFact
-                              , fact_do_logging = True }
+findClosures :: Body InstrNode -> FuelMonad (FactBase ProcFact)
+findClosures body = do
+  let bwd = BwdPass { bp_lattice = findCloLat
+                    , bp_transfer = findCloTrans
+                    , bp_rewrite = findCloRewrite }
+  analyzeAndRewriteBwd bwd body (mkFactBase []) >>= return . snd
+
+findCloLat :: DataflowLattice ProcFact
+findCloLat = DataflowLattice { fact_bot = NoInfo
+                             , fact_name = "Find procedures that return closures."
+                             , fact_extend = extendFact
+                             , fact_do_logging = False }
   where
-    extendFact _ (OldFact old@(CloFact _ oldRef oldAll)) (NewFact new@(CloFact _ newRef newAll)) 
-      | oldRef == newRef && oldAll == newAll = (NoChange, old)
-      | otherwise = let ref' = newRef `Map.union` oldRef
-                        all' = newAll `Map.union` oldAll
-                    in (SomeChange, new { closureRefs = ref'
-                                        , allClosures = all' })
-                                           
-
-cloTransfer :: FwdTransfer InstrNode CloFact
--- Initialize analysis for this block when we see a label.
-cloTransfer (EntryLabel (G l) _ hl) f = 
-  let fact = fromMaybe emptyFact $ lookupFact f hl
-  in fact { currLabel = Just (l, hl) } 
-cloTransfer (LabelNode _ (N l) hl) f = 
-  let fact = fromMaybe emptyFact $ lookupFact f hl
-  in fact { currLabel = Just (l, hl) }
-
--- Add teh value of the result register to the map of labels to
--- closures. If a closure was allocated in this register, and it has
--- not been overwritten, then it will have the value MkClo. Otherwise,
--- it will the value Other.
-cloTransfer (Ret (H.Ret _)) f@(CloFact { currLabel = Just (curr, hCurr)
-                                       , closureRefs
-                                       , allClosures }) 
-  = case Map.lookup curr closureRefs of
-      Just cloInfo@(MkClo _ _) -> mkFactBase [(hCurr, f { allClosures = Map.insert curr cloInfo allClosures })]
-      _ -> mkFactBase []
-
-cloTransfer (Open (H.MkClo reg _ regs)) f@(CloFact { closureRefs }) = 
-  let cloInfo = MkClo reg (length regs)
-  in f { closureRefs = Map.insert reg cloInfo closureRefs } 
-
--- Transfer our facts to target labels.
-cloTransfer (Jmp _ hl) f@(CloFact { currLabel = Just (curr, hCurr)
-                                  , closureRefs
-                                  , allClosures }) 
-  = case Map.lookup curr closureRefs of
-      Just cloInfo@(MkClo _ _) -> 
-        let newFact = f { allClosures = Map.insert curr cloInfo allClosures } 
-        in mkFactBase [(hl, newFact)
-                      , (hCurr, newFact)]
-      _ -> mkFactBase []
--- Transfer our facts to target labels.
-cloTransfer (FailT _ (F fl) (T tl)) f@(CloFact { currLabel = Just (curr, hCurr)
-                                               , closureRefs
-                                               , allClosures }) 
-  = case Map.lookup curr closureRefs of
-      Just cloInfo@(MkClo _ _) -> 
-        let newFact = f { allClosures = Map.insert curr cloInfo allClosures } 
-        in mkFactBase [(fl, newFact)
-                      , (tl, newFact)
-                      , (hCurr, newFact)]
-      _ -> mkFactBase []
-
--- These instructions destroy any information
--- we have about a register - unknown if it is a function after
--- this.
-cloTransfer (Open instr) f@(CloFact { closureRefs })
-  | isJust dest = f { closureRefs = Map.insert (fromJust dest) Other closureRefs }
-  | otherwise = f
-    where
-      dest = case instr of
-               H.Enter _ _ d -> Just d
-               H.AllocD d _ _ -> Just d
-               H.Copy _ d -> Just d
-               H.Store _ (d, _) -> Just d
-               H.Set d _ -> Just d
-               _ -> Nothing
-
--- These conditions don't matter when we don't have a current label.
-cloTransfer (Ret _) _ = mkFactBase []
-cloTransfer (Jmp _ _) _ = mkFactBase []
-cloTransfer (FailT _ _ _) _ = mkFactBase []
-cloTransfer (Halt _) _ = mkFactBase []
-cloTransfer (Error _) _ = mkFactBase []
+    extendFact :: Label -> OldFact ProcFact -> NewFact ProcFact -> (ChangeFlag, ProcFact)
+    extendFact _ (OldFact old) (NewFact new) = 
+      let p = procMeet old new
+      in (if p == new then NoChange else SomeChange
+         , p)
 
 
-cloRewrite :: FwdRewrite InstrNode CloFact
-cloRewrite = shallowFwdRw rewrite
-  where
-    rewrite :: SimpleFwdRewrite InstrNode CloFact
-    rewrite i@(Open (H.MkClo _ _ _)) f =
-      Just $ catGraphs [mkMiddle (Open (H.Note (show f)))
-                       , mkMiddle i]
-    rewrite i@(Ret _) f = Just $ mkMiddle (Open (H.Note (show f))) <*> mkLast i
-    rewrite _ _ = Nothing
+findCloTrans :: BwdTransfer InstrNode ProcFact
+-- Return what we've found out so far.
+findCloTrans (EntryLabel _ _ l) f = mkFactBase [(l, f)]
+findCloTrans (LabelNode _ _  l) f = mkFactBase [(l, f)]
+-- Finding a return instruction lets us know what 
+-- register is returned from this procedure. Next
+-- need to figure out what value it can hold
+findCloTrans (Ret r) _ = Proc r Unknown
+-- Combine facts from both branches to determine
+-- if our return register remains unchanged.
+findCloTrans (FailT _ (F fl) (T tl)) f =
+  let fp = fromMaybe NoInfo $ lookupFact f fl
+      tp = fromMaybe NoInfo $ lookupFact f tl
+  in procMeet tp fp
+-- Transfer facts known so far from destination label
+findCloTrans (Jmp _ l) f = fromMaybe NoInfo $ lookupFact f l
+-- Nothing known yet in these cases.
+findCloTrans (Halt _) _ = NoInfo
+findCloTrans (Error _) _ = NoInfo
+-- Determine if this MkClo instruction affects our destination
+-- register and, if so, if that overrides information already
+-- known or tells us that the procedure returns a closure.
+findCloTrans (Open (H.MkClo dest lab regs)) f@(Proc r _)  
+  | dest == r = procMeet f (Proc dest (Clo lab (length regs)))
+findCloTrans (Open (H.Enter _ _ dest)) (Proc r _) 
+  | dest == r = Proc r Other
+findCloTrans (Open (H.AllocD dest _ _)) (Proc r _) 
+  | dest == r = Proc r Other
+findCloTrans (Open (H.Copy _ dest)) (Proc r _) 
+  | dest == r = Proc r Other
+findCloTrans (Open (H.Load _ dest)) (Proc r _)
+  | dest == r = Proc r Other
+findCloTrans (Open (H.Set dest _)) (Proc r _) 
+  | dest == r = Proc r Other
+findCloTrans (Open _) f = f
+
+findCloRewrite :: forall n e x f. n e x -> Fact x f -> Maybe (BwdRes n f e x)
+findCloRewrite = noBwdRewrite
+
+-- | Combines Proc values such that 
+-- any ambiguity in the value held by the register
+-- results in a ``bottom'' (i.e., @Other@) value.
+procMeet :: ProcFact -> ProcFact -> ProcFact
+procMeet (Proc r v) (Proc r' v') 
+  | r == r' = if v == v'
+              then Proc r v
+              else case (v, v') of
+                     (Unknown, _) -> Proc r v'
+                     (_, Unknown) -> Proc r v
+                     (Other, _) -> Proc r Other
+                     (_, Other) -> Proc r Other
+                     _ -> Proc r Other
+  | otherwise = error $ "procMeet: return registers should not differ: " ++ show (r, r')
+procMeet p p' = if p == NoInfo
+                then p'
+                else p
