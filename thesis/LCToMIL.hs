c6b6800eb16a84dfa96115a4267101be8761e040
Justin Bailey <justinb@cs.pdx.edu>
2011-02-03 21:12:21 -0800

    First cut at LambdaCase
    Copied compiler from Lambda3 to LCToMIL and
    converted pattern matches.

---
+++ thesis/LCToMIL.hs
@@ -0,0 +1,167 @@
+{-# LANGUAGE TypeSynonymInstances, GADTs, RankNTypes
+  , NamedFieldPuns, TypeFamilies, ScopedTypeVariables #-}
+
+module LCToMIL
+
+where
+
+import Control.Monad.State (State, execState, modify, gets)
+import Text.PrettyPrint 
+import Data.List (sort)
+import Data.Maybe (fromMaybe, isJust, catMaybes)
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Compiler.Hoopl
+
+import MIL
+import Syntax.LambdaCase hiding (Alt)
+import qualified Syntax.LambdaCase as LC
+import Util
+
+-- | Compiler state. 
+data CompS = C { compI :: Int -- ^ counter for fresh variables
+               , compG :: (ProgM C C) -- ^ Program control-flow graph.
+               , compT :: [Name] } -- ^ top-level function names.
+               
+type CompM = State CompS
+
+instance UniqueMonad CompM where
+  freshUnique = freshVal
+
+compileStmtM :: Expr 
+  -> (TailM -> CompM (ProgM O C))
+  -> CompM (ProgM O C)
+
+compileStmtM (EVar v) ctx 
+  = ctx (Return v)
+
+compileStmtM (EPrim _ _) _ 
+  = error "EPrim statement not implemented."
+
+compileStmtM (ELit _) _ 
+  = error "ELit statement not implemented."
+
+compileStmtM (ECon cons _ exprs) ctx = 
+  let compExpr vs [] = ctx (ConstrM cons (reverse vs))
+      compExpr vs (e:es) = compVarM e $ \v -> 
+                           compExpr (v:vs) es
+  in compExpr [] exprs
+
+compileStmtM (ELam v _ b) ctx = do
+  let fvs = free b [v]
+  f <- do
+    prog <- compileStmtM b (return . mkLast . Done)
+    name <- newTop "absBody"
+    cloDefn name v fvs prog 
+  ctx (Closure f fvs)
+
+compileStmtM (ELet _ _) _ 
+  = error "ELet statement not implemented."
+
+compileStmtM (ECase e lcAlts) ctx = do
+  let alts = map (\(LC.Alt cons _ vs expr) -> Alt cons vs expr) lcAlts
+  r <- fresh "result"
+  f <- ctx (Return r) >>= callDefn "caseJoin" 
+  let compAlt (Alt cons vs body) = do
+        body' <- compileStmtM body (mkBind r f) >>= callDefn ("altBody" ++ cons)
+        return (Alt cons vs body')
+  altsM <- mapM compAlt alts
+  compVarM e $ \v -> return (mkLast (CaseM v altsM))
+  where
+    callDefn :: String -> ProgM O C -> CompM TailM
+    callDefn name body = do 
+      f <- newTop name
+      ts <- gets compT
+      dest <- blockDefn f [] body
+      return (Goto dest [])
+
+compileStmtM (EApp e1 e2) ctx 
+  = compVarM e1 $ \f ->
+    compVarM e2 $ \g ->
+      ctx (Enter f g)
+
+compileStmtM (EFatbar _ _) _ 
+  = error "EFatbar not implemented."
+
+compileStmtM (EBind _ _ _ _) _ 
+  = error "EBind not implemented."
+
+compVarM :: Expr 
+  -> (Name -> CompM (ProgM O C))
+  -> CompM (ProgM O C)
+compVarM (EVar v) ctx = ctx v
+compVarM (EPrim p _) ctx = ctx p
+compVarM (ELit _) _ = error "ELit not implemented."
+compVarM e ctx = compileStmtM e $ \t -> do
+  v <- fresh "v"
+  rest <- ctx v
+  return (mkMiddle (v `Bind` t) <*> rest)
+
+mkBind r f t = return (mkMiddle (Bind r t) <*> mkLast (Done f))
+
+free :: Expr -> [Name] -> [Name]
+free = undefined
+      
+-- | Create a fresh variable with the given
+-- prefix.
+fresh :: String -> CompM String
+fresh prefix = do
+  i <- gets compI
+  modify (\s@(C { compI }) -> s { compI = compI + 1})
+  return (prefix ++ show i)
+
+-- | Create a new unique value; used in the
+-- instance declaration for (UniqueMonad CompM).
+freshVal :: CompM Unique
+freshVal = do
+  i <- gets compI
+  modify (\s@(C { compI }) -> s { compI = compI + 1})
+  return (intToUnique i)
+
+-- | Make a new top-level function name, based on the
+-- prefix given.
+newTop :: Name -> CompM Name
+newTop name = do
+  f <- fresh name
+  addName f
+  return f
+
+-- | Add a new block.
+blockDefn :: Name -> [Name] -> ProgM O C -> CompM Dest
+blockDefn name args progM = withNewLabel $ \l -> do
+  addProg (mkFirst (BlockEntry name l args) <*> progM)
+  return (name, l)
+  
+-- | Add a new closure-capturing block.
+cloDefn :: Name -> Name -> [Name] -> ProgM O C -> CompM Dest
+cloDefn name arg clos progM 
+  | not (isCapture progM) = withNewLabel $ \l -> do
+                          let args = clos ++ [arg]
+                          dest <- blockDefn ("block" ++ name) args progM
+                          addProg (mkFirst (CloEntry name l clos arg) <*>
+                                   mkLast (Done (Goto dest args)))
+                          return (name, l)
+  | otherwise = withNewLabel $ \l -> do
+                  addProg (mkFirst (CloEntry name l clos arg) <*> progM)
+                  return (name, l)
+  where
+    isCapture :: ProgM O C -> Bool
+    isCapture (GMany (JustO block) _ _ ) = 
+      case blockToNodeList' block of
+        (_, [], (JustC (Done (Closure _ _)))) -> True
+        _ -> False
+
+-- | Add a program (C C block) to the list of blocks
+-- maintained by the monad.
+addProg :: ProgM C C -> CompM ()
+addProg block = modify (\s@(C { compG }) -> s { compG = block |*><*| compG })
+
+-- | Do something with a new label.
+withNewLabel :: UniqueMonad m => (Label -> m a) -> m a
+withNewLabel f = freshLabel >>= f
+  
+-- | Add a name to the list of top-level functions.
+addName :: Name -> CompM ()
+addName name = modify (\s@(C { compT }) -> s { compT = name : compT })
