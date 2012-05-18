4ed76f5b2f1c65c9310a6dbda8fa270b9fdb1469
Justin Bailey <justinb@cs.pdx.edu>
2012-05-18 15:27:49 -0700

    Commit history analysis.

---
+++ thesis/analysis.rb
@@ -0,0 +1,5 @@
+# git log --before="April 24, 2012" --author="Bailey" --date=short --pretty=format:"%ad, %s" --numstat --no-merges
+
+# git log --before="April 24, 2012" --author="Bailey" --date=short --pretty=format:"%ad" > dates.txt
+
+# git log --before="April 24, 2012" --author="Bailey" --date=short --pretty=format:"" --numstat --no-merges
